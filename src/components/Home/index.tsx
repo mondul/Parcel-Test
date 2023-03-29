44e7a40767c0100f458fcdf2707c0eb7e181b52f
Armando G. Mondul <mondul@huyzona.com>
2023-03-29 17:49:42 -0500

    Add some content to home page

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,4 +1,10 @@
 
 export default function Home() {
-  return <h1>Home</h1>
+  return <>
+    <h1>Home</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore corporis, eos fuga atque voluptatum praesentium architecto repellendus? Rem fugiat ipsa debitis pariatur voluptatibus explicabo, aspernatur delectus earum consectetur maiores corrupti!</p>
+    <p>Necessitatibus suscipit, odit, assumenda distinctio corrupti veritatis officiis cupiditate consequuntur culpa facere numquam laborum explicabo recusandae sunt. Sit asperiores excepturi eum aliquid eos provident expedita quod, aperiam doloremque, ullam nisi?</p>
+    <p>Earum voluptas vel quod in deserunt impedit tenetur. Sit, aperiam, ab saepe reprehenderit animi, non quia quisquam numquam architecto commodi quasi nulla deleniti. Nam esse sapiente odio ut accusantium minima.</p>
+    <p>Ab veniam tenetur animi aspernatur asperiores assumenda provident sit reprehenderit labore minima amet facere voluptas odit cum esse officiis maxime, saepe adipisci eligendi magnam harum perspiciatis! Eveniet, quisquam. Reprehenderit, veniam.</p>
+  </>
 }
